aee7e5b0c14e23b1c7351f7e9a2d3a0411967e0d
kjdiaz508 <diazkevin508@gmail.com>
2024-09-05 23:42:34 -0700

    added type hints

--- main.py
+++ main.py
@@ -1,24 +1,61 @@
 import pygame as pg
 
 
+class Game:
+    def __init__(self):
+        pg.init()
+        self.WIDTH, self.HEIGHT = 800, 600
+        self.BACKGROUND_COLOR = (255, 77, 77)
+        self.canvas = pg.Surface((self.WIDTH, self.HEIGHT))
+        self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.font = pg.font.Font(None, 20)
+        self.clock = pg.time.Clock()
+        self.running, self.playing = True, True
+        self.state_stack: list[State] = [Title(self)]
+
+    def game_loop(self):
+        while self.running:
+            self.get_events()
+            self.update()
+            self.render()
+            self.clock.tick(60)
+
+    def get_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+            self.state_stack[-1].events(event)
+
+    def update(self):
+        self.state_stack[-1].update()
+
+    def render(self):
+        self.state_stack[-1].render(self.canvas)
+        self.screen.blit(self.canvas, (0, 0))
+        pg.display.flip()
+
+    def draw_text(self, surface: pg.Surface, text: str, position: tuple[int, int]):
+        pass
+
+
 class State:
-    def __init__(self, game):
+    def __init__(self, game: Game):
         self.game = game
 
     def update(self):
         pass
 
-    def events(self, event):
+    def events(self, event: pg.event.Event):
         pass
 
-    def render(self, display):
+    def render(self, display: pg.Surface):
         pass
 
     def enter_state(self):
-        self.game.push_state(self)
+        self.game.state_stack.append(self)
 
     def exit_state(self):
-        self.game.pop_state()
+        self.game.state_stack.pop()
 
 
 class Title(State):
@@ -54,49 +91,6 @@ class GamePlay(State):
         display.fill((77, 77, 77))
 
 
-class Game:
-    def __init__(self):
-        pg.init()
-        self.WIDTH, self.HEIGHT = 800, 600
-        self.BACKGROUND_COLOR = (255, 77, 77)
-        self.canvas = pg.Surface((self.WIDTH, self.HEIGHT))
-        self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))
-        self.clock = pg.time.Clock()
-        self.running, self.playing = True, True
-        self.state_stack = [Title(self)]
-
-    def get_state(self):
-        return self.state_stack[-1]
-
-    def push_state(self, state):
-        self.state_stack.append(state)
-
-    def pop_state(self):
-        self.state_stack.pop()
-
-    def game_loop(self):
-        while self.running:
-            self.get_events()
-            self.update()
-            self.render()
-            self.clock.tick(60)
-
-    def get_events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.running = False
-            self.get_state().events(event)
-
-    def update(self):
-        self.get_state().update()
-
-    def render(self):
-        self.get_state().render(self.canvas)
-        self.screen.blit(self.canvas, (0, 0))
-        pg.display.flip()
-
-
 if __name__ == "__main__":
     g = Game()
     g.game_loop()
-
